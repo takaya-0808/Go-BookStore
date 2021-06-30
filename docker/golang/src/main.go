09bd87bdbd8e914e7031c5963512e3248e8e8b4b
e175751@ie.u-ryukyu.ac.jp <e175751@ie.u-ryukyu.ac.jp>
2021-06-30 14:20:29 +0900

    add parameter

--- docker/golang/src/main.go
+++ docker/golang/src/main.go
@@ -16,5 +16,16 @@ func main() {
 			"message": "pong",
 		})
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/get_message/:name", func(c *gin.Context) {
+			name := c.Param("name")
+			message := "name is " + name
+			c.JSON(http.StatusOK, gin.H{
+				"name": message,
+			})
+		})
+		// v1.GET("/read_message/:name", func(c *gin.Context){})
+	}
 	r.Run(":8018")
 }
