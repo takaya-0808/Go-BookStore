2994d2865ef5f69e5d9c3d04089ba1a810e9bad4
takaya <takaya1912@gmail.com>
2021-07-01 23:28:58 +0900

    fix

--- docker/golang/src/main.go
+++ docker/golang/src/main.go
@@ -25,17 +25,7 @@ func main() {
 	{
 		v1.GET("/get_message/:name", GetMessage)
 
-		v1.GET("/read_message/:name", func(c *gin.Context) {
-			var msg struct {
-				Name    string `json:"user"`
-				Message string
-				Number  int
-			}
-			msg.Name = c.Param("name")
-			msg.Message = "これは構造体をJSONで返すためのテストです。"
-			msg.Number = 1111
-			c.JSON(http.StatusOK, msg)
-		})
+		v1.GET("/read_message/:name", ReadMessage)
 
 		v1.GET("/country/:id", func(c *gin.Context) {
 			id := c.Param("id")
@@ -66,4 +56,29 @@ func GetMessage(c *gin.Context) {
 	name := c.Param("name")
 	message := "name is " + name
 	c.JSON(http.StatusOK, gin.H{"name": message})
+}
+
+func ReadMessage(c *gin.Context) {
+	var msg struct {
+		Name    string `json:"user"`
+		Message string
+		Number  int
+	}
+	msg.Name = c.Param("name")
+	msg.Message = "これは構造体をJSONで返すためのテストです。"
+	msg.Number = 1111
+	c.JSON(http.StatusOK, msg)
+}
+
+func PrintID(c *gin.Context) {
+	id := c.Param("id")
+	log.Println(id)
+}
+
+func GetCountry(c *gin.Context) {
+	var country Country
+	id := c.Param("id")
+	country.Code = id
+	country.Name = "japan"
+	c.JSON(http.StatusOK, country)
 }
