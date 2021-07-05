eb62ac5b3966c4dc749c20f0899f5fcf703cacd8
takaya <takaya1912@gmail.com>
2021-07-05 22:55:29 +0900

    fix router

--- docker/golang/src/main.go
+++ docker/golang/src/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"src/service/controller"
+	"src/app"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,23 +15,5 @@ type Country struct {
 func main() {
 	log.Println("Start server ...................")
 	router := gin.Default()
-	// app.InitRouting(r)
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	v1 := router.Group("/v1")
-	{
-		v1.GET("/get_message/:name", controller.GetMessage)
-
-		v1.GET("/read_message/:name", controller.ReadMessage)
-
-		v1.GET("/country/:id", controller.PrintID)
-
-		v1.GET("/Country/:id", controller.GetCountry)
-
-		v1.POST("/Country/", controller.PostCountry)
-	}
-	router.Run(":8018")
+	app.InitRouting(router)
 }
