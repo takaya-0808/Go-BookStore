4a2f24ee5460c04eba6f149bbc98c4748d311e15
takaya <takaya1912@gmail.com>
2021-07-07 14:41:24 +0900

    add UserModel

---
+++ docker/golang/src/domain/model/book.go
@@ -0,0 +1,38 @@
+package model
+
+type Books []Book
+
+type Book struct {
+	ID     int    `json:"id" gorm:"primary_key"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+type User struct {
+	ID    int    `json:"id" gorm:"primary_key"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Pass  string `json:"pass"`
+}
+
+type CreateBook struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+type UpdateBook struct {
+	ID     int    `json:"id" gorm:"primary_key"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+type Country struct {
+	Code string
+	Name string
+}
+
+type UserModel struct {
+	UserName string
+	PassWord string
+	Email    string
+}
