3444b19931f00e22ac5087fb8781df5c12cb44c2
takaya <takaya1912@gmail.com>
2021-07-09 11:18:12 +0900

    fix

--- docker/golang/src/domain/model/book.go
+++ docker/golang/src/domain/model/book.go
@@ -1,32 +1,21 @@
 package model
 
+import (
+	"time"
+)
+
 type Books []Book
 
 type Book struct {
-	ID     int    `json:"id" gorm:"primary_key"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+	ID        int       `json:"id" gorm:"primary_key"`
+	Title     string    `json:"title"`
+	Context   string    `json:"content"`
+	Author    string    `json:"author"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
-type User struct {
-	ID    int    `json:"id" gorm:"primary_key"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Pass  string `json:"pass"`
-}
-
-type CreateBook struct {
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-
-type UpdateBook struct {
-	ID     int    `json:"id" gorm:"primary_key"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-}
-
-type msg struct {
+type Msg struct {
 	Name    string `json:"user"`
 	Message string
 	Number  int
@@ -38,7 +27,7 @@ type Country struct {
 }
 
 type UserModel struct {
-	UserName string
-	PassWord string
-	Email    string
+	UserName string `json:"username"`
+	PassWord string `json:"password"`
+	Email    string `json:"email"`
 }
