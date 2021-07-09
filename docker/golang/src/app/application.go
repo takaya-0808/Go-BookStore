218fac1bbd32d0b8891150d2dc1a82bb9d8d4751
takaya <takaya1912@gmail.com>
2021-07-10 00:01:15 +0900

    add GET/POST/PUT/DELETE

--- docker/golang/src/app/application.go
+++ docker/golang/src/app/application.go
@@ -10,6 +10,8 @@ func InitRouting(router *gin.Engine) {
 
 	// bookContoroller := controller.NewBookRepository(NewSqlHandler())
 
+	handler := controller.BookHandler
+
 	bookEngine := router.Group("/api")
 	{
 		v1 := bookEngine.Group("/v1")
@@ -20,6 +22,11 @@ func InitRouting(router *gin.Engine) {
 			v1.GET("/Country/:id", func(c *gin.Context) { controller.GetCountry(c) })
 			v1.POST("/Country/", func(c *gin.Context) { controller.PostCountry(c) })
 			v1.POST("/loginJSON/", func(c *gin.Context) { controller.PostLogin(c) })
+			v1.GET("/Book", func(c *gin.Context) { handler.View(c) })
+			v1.GET("/Book/:id", func(c *gin.Context) { handler.Search(c) })
+			v1.POST("/Book/:id", func(c *gin.Context) { handler.Create(c) })
+			v1.PUT("/Book/:id", func(c *gin.Context) { handler.Update(c) })
+			v1.DELETE("/Book/:id", func(c *gin.Context) { handler.Delete(c) })
 		}
 	}
 	router.Run(":8018")
