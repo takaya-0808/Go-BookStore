07ac0f8133b27ea266902a08b07fd551b6b070db
takaya <takaya1912@gmail.com>
2021-07-11 12:50:25 +0900

    fix

--- docker/golang/src/service/controller/book.go
+++ docker/golang/src/service/controller/book.go
@@ -1,16 +1,14 @@
 package controller
 
 import (
-	"log"
 	"net/http"
-	"src/domain/model"
+//	"src/domain/model"
 	"src/usecase"
-
 	"github.com/gin-gonic/gin"
 )
 
 type BookHandler struct {
-	bookUsecase usecase.BookRepository
+	bookUsecase usecase.bookUsecase
 }
 
 func (handler *BookHandler) View(c *gin.Context) {
@@ -31,11 +29,11 @@ func (handler *BookHandler) Update(c *gin.Context) {}
 
 func (handler *BookHandler) Delete(c *gin.Context) {}
 
-func GetMessage(c *gin.Context) {
-	name := c.Param("name")
-	message := "name is " + name
-	c.JSON(http.StatusOK, gin.H{"name": message})
-}
+// func GetMessage(c *gin.Context) {
+// 	name := c.Param("name")
+// 	message := "name is " + name
+// 	c.JSON(http.StatusOK, gin.H{"name": message})
+// }
 
 func ReadMessage(c *gin.Context) {
 	var msg model.Msg
@@ -45,40 +43,40 @@ func ReadMessage(c *gin.Context) {
 	c.JSON(http.StatusOK, msg)
 }
 
-func PrintID(c *gin.Context) {
-	id := c.Param("id")
-	log.Println(id)
-}
+// func PrintID(c *gin.Context) {
+// 	id := c.Param("id")
+// 	log.Println(id)
+// }
 
-func GetCountry(c *gin.Context) {
-	var country model.Country
-	id := c.Param("id")
-	country.Code = id
-	country.Name = "japan"
-	c.JSON(http.StatusOK, country)
-}
+// func GetCountry(c *gin.Context) {
+// 	var country model.Country
+// 	id := c.Param("id")
+// 	country.Code = id
+// 	country.Name = "japan"
+// 	c.JSON(http.StatusOK, country)
+// }
 
-func PostCountry(c *gin.Context) {
-	var country model.Country
-	if err := c.BindJSON(&country); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
-		return
-	}
-	log.Println(country.Code)
-	c.JSON(http.StatusCreated, gin.H{"mesaage": "ok"})
-}
+// func PostCountry(c *gin.Context) {
+// 	var country model.Country
+// 	if err := c.BindJSON(&country); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+// 		return
+// 	}
+// 	log.Println(country.Code)
+// 	c.JSON(http.StatusCreated, gin.H{"mesaage": "ok"})
+// }
 
-func PostLogin(c *gin.Context) {
-	var usermodel model.UserModel
-	if c.BindJSON(&usermodel) == nil {
-		if usermodel.UserName == "hoge" && usermodel.PassWord == "password" {
-			c.JSON(http.StatusOK, gin.H{
-				"status": "you are logged in",
-			})
-		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{
-				"status": "unauthorized",
-			})
-		}
-	}
-}
+// func PostLogin(c *gin.Context) {
+// 	var usermodel model.UserModel
+// 	if c.BindJSON(&usermodel) == nil {
+// 		if usermodel.UserName == "hoge" && usermodel.PassWord == "password" {
+// 			c.JSON(http.StatusOK, gin.H{
+// 				"status": "you are logged in",
+// 			})
+// 		} else {
+// 			c.JSON(http.StatusUnauthorized, gin.H{
+// 				"status": "unauthorized",
+// 			})
+// 		}
+// 	}
+// }
