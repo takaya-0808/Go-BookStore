840eefa97629c213ab0f4082ec26cb9ad0cf20ea
takaya <takaya1912@gmail.com>
2021-07-12 16:00:44 +0900

    rm book struct

--- docker/golang/src/domain/model/book.go
+++ docker/golang/src/domain/model/book.go
@@ -1,20 +1,5 @@
 package model
 
-import (
-	"time"
-)
-
-type Books []Book
-
-type Book struct {
-	ID        int       `json:"id" gorm:"primary_key"`
-	Title     string    `json:"title"`
-	Context   string    `json:"content"`
-	Author    string    `json:"author"`
-	UpdatedAt time.Time `json:"updated_at"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
 type Msg struct {
 	Name    string `json:"user"`
 	Message string
