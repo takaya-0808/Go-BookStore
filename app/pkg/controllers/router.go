149a9ceb2228623ad9d8e03abd6c2b16897826b1
takaya <takaya1912@gmail.com>
2021-07-18 00:39:15 +0900

    add

--- app/pkg/controllers/router.go
+++ app/pkg/controllers/router.go
@@ -8,9 +8,9 @@ func SetUp(Router *gin.RouterGroup) {
 	books := Router.Group("/book")
 	{
 		bc := BookController{}
-		books.GET("", bc.Search)
-		books.GET("/:id", bc.View)
-		// books.POST("")
+		books.GET("", bc.View)
+		books.GET("/:id", bc.Search)
+		// books.POST("", bc.Add)
 		// books.PUT("/:id")
 		// books.DELETE("/:id")
 	}
